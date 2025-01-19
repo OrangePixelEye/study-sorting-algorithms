f2139d7230edd76ab0079b60aebe7fa429165664
Felipe Batista <felipe.batistaa52@gmail.cpm>
2025-01-19 12:37:23 -0300

    Binary search recursive

--- algorithms/binary_search.go
+++ algorithms/binary_search.go
@@ -1,15 +1,14 @@
 package algorithms
 
 func BinarySearch(arr []int, target int) int {
-	var length = len(arr)
-	var left = 0
-	var right = length - 1
+	left := 0
+	right := len(arr) - 1
 	for left <= right {
-		var mid = (left + right) / 2
+		mid := (left + right) / 2
 		if arr[mid] == target {
 			return mid
 		} else if arr[mid] < target {
-			left = mid
+			left = mid + 1
 		} else {
 			right = mid - 1
 		}
@@ -17,6 +16,20 @@ func BinarySearch(arr []int, target int) int {
 	return -1
 }
 
-func BinarySearchRecursive() {
+func BinarySearchRecursiveHelper(arr []int, target int) int {
+	return binarySearchRecursive(arr, target, 0, len(arr)-1)
+}
 
+func binarySearchRecursive(arr []int, target int, left int, right int) int {
+	mid := (left + right) / 2
+	for left <= right {
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] > target {
+			return binarySearchRecursive(arr, target, left-1, mid)
+		} else {
+			return binarySearchRecursive(arr, target, mid+1, right)
+		}
+	}
+	return -1
 }
--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	var result = algorithms.BinarySearch([]int{1, 2, 32, 35, 46, 55, 233}, 46)
+	var result = algorithms.BinarySearchRecursiveHelper([]int{1, 2, 32, 35, 46, 55, 233}, 233)
 	fmt.Println(result)
 }
